f91911760f5b832f5c889b28b719e9c51cc13843
CamillaKurup <4342295@RTS-365.dk>
2017-08-25 12:30:08 +0200

    de

---
+++ funktioner.md
@@ -0,0 +1,9 @@
+# Beskrivelse af funktioner
+
+```js
+
+function hop (){} // Her definere vi en funktion og tildeler den derved en egenskab
+
+```
+
+Funktioner kan oprettes spottes på flere måder. Nede for viser jeg flere eksempler
---
+++ test.js
@@ -0,0 +1,29 @@
+// function hilsPaa (navn)
+// {
+//     console.log ("hej med dig, " + navn);
+// }
+
+// function hils ()
+// {
+//     console.log ("hej hej!");
+// }
+
+
+// hilsPaa ("Camilla");
+// hils();
+
+
+// function givMigDetDobelteAf(tal){
+//     console.log(tal *2);
+// }
+// givMigDetDobelteAf(8);
+
+
+
+
+
+
+
+
+
+
